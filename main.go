ce5e46246bd782a8745c451512ecd48eb5a584f8
Hunter Long <info@socialeck.com>
2017-03-27 20:57:58 -0600

    first

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"log"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"math"
+)
+
+
+var conn *ethclient.Client
+
+
+func init() {
+
+	var err error
+	// Create an IPC based RPC connection to a remote node
+
+	conn, err = ethclient.Dial("/Users/hunter/Library/Ethereum/geth.ipc")
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+
+	// CJX
+	//reqAddress := "0x38122af92aa2819Cdb2bE029f2Fa325959B52Ec8";
+	//reqContract := "0x2ce349291b8365f8d12c4cedf992969f680c726e";
+
+	// DAO
+	//reqAddress := "0x21314ff1669aced72b3c72ad912102186cf5e1cd";
+	//reqContract := "0x48c80F1f4D53D5951e5D5438B54Cba84f29F32a5";
+
+}
+
+func GetAccount(contract string, wallet string) (string, float64, string, uint8) {
+	var err error
+
+	token, err := NewTokenCaller(common.HexToAddress(contract), conn)
+	if err != nil {
+		log.Fatalf("Failed to instantiate a Token contract: %v", err)
+	}
+
+	address := common.HexToAddress(wallet)
+	balance, err := token.BalanceOf(nil, address)
+	symbol, err := token.Symbol(nil)
+	decimals, err := token.Decimals(nil)
+	name, err := token.Name(nil)
+
+	if err != nil {
+		log.Fatalf("Failed to retrieve token name: %v", err)
+	}
+
+	z := math.Pow(0.1,float64(decimals))
+	newBalance := float64(balance.Int64()) * z
+
+	return name, newBalance, symbol, decimals
+
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num * output)) / output
+}
---
+++ server.go
@@ -0,0 +1,51 @@
+package main
+
+
+import (
+	"github.com/gorilla/mux"
+	"net/http"
+	"encoding/json"
+	"fmt"
+)
+
+
+type BalanceResponse struct {
+	Name	string	`json:"name"`
+	Wallet	string	`json:"wallet"`
+	Balance	float64	`json:"balance"`
+	Symbol	string	`json:"symbol"`
+	Decimals	uint8	`json:"decimals"`
+}
+
+func getMembersHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	vars := mux.Vars(r)
+	contract := vars["contract"]
+	wallet := vars["wallet"]
+
+	fmt.Println("Fetching Wallet ",wallet," at contract ",contract)
+
+	name, balance, token, decimals := GetAccount(contract,wallet)
+
+	new := BalanceResponse{
+		Name: name,
+		Wallet: wallet,
+		Balance: balance,
+		Symbol: token,
+		Decimals: decimals,
+	}
+
+	j, _ := json.Marshal(new)
+	w.Write(j)
+}
+
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/balance/{contract}/{wallet}", getMembersHandler).Methods("GET")
+
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
+}
+
