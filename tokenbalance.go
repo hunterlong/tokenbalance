4d08c64b81f705dd598138d1e89a12b7b30708cb
lylyl <liyunlong518@gmail.com>
2019-10-10 17:04:05 +0800

    Update tokenbalance.go
    fix decimal bug

--- tokenbalance.go
+++ tokenbalance.go
@@ -155,20 +155,9 @@ func bigIntFloat(balance *big.Int, decimals int64) *big.Float {
 	if balance.Sign() == 0 {
 		return big.NewFloat(0)
 	}
-	bal := big.NewFloat(0)
-	bal.SetInt(balance)
-	pow := bigPow(10, decimals)
-	p := big.NewFloat(0)
-	p.SetInt(pow)
-	bal.Quo(bal, p)
-	return bal
-}
 
-func bigPow(a, b int64) *big.Int {
-	r := big.NewInt(a)
-	return r.Exp(r, big.NewInt(b), nil)
+	return new(big.Float).Quo(new(big.Float).SetInt(balance), new(big.Float).SetInt(math.BigPow(10, decimals)))
 }
-
 func clean(newNum string) string {
 	stringBytes := bytes.TrimRight([]byte(newNum), "0")
 	newNum = string(stringBytes)
